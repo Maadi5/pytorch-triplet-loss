ded307fb1008c06e0def5d4f4a639952c9790bdf
Maadi5 <ma.adithya55@gmail.com>
2022-01-13 13:43:49 +0530

    adding utils, factoring models

--- TNN/modified_resnet.py
+++ TNN/modified_resnet.py
@@ -3,9 +3,6 @@ import torch.nn as nn
 # from .utils import load_state_dict_from_url
 
 
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
-           'resnet152', 'resnext50_32x4d', 'resnext101_32x8d',
-           'wide_resnet50_2', 'wide_resnet101_2']
 
 
 model_urls = {
--- TNN/util.py
+++ TNN/util.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class AverageMeter(object):
 	"""Computes and stores the average and current value"""
@@ -16,3 +16,31 @@ class AverageMeter(object):
 		self.sum += val * n
 		self.count += n
 		self.avg = self.sum / self.count
+
+
+def split_label_class(labels, num_split):
+	labels.pop(labels.index('N/A'))
+	set1 = random.sample(labels, num_split)
+	set2 = []
+	for i in labels:
+		if i not in set1:
+			set2.append(i)
+	return set1, set2
+
+
+def get_lbl_val(classes_all, subset):
+	s2 = []
+	for i in subset:
+		s2.append(classes_all.index(i))
+	return s2
+
+
+def find_index_elementlist(main_arr, id_list):
+	index_lbl = []
+	main_arr = np.array(main_arr)
+	for ind, g in enumerate(id_list):
+		ar = np.where(main_arr == g)
+		ar_l = ar[0].tolist()
+		index_lbl.extend(ar_l)
+	return index_lbl
+
